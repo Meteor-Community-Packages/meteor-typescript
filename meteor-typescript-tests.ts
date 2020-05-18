c6c25ceb80db4d47673362517bf10cc0f5b6f4d6
Per Bergland <per@refapp.se>
2020-05-18 20:16:37 +0200

    trying to add a compiler plugin

--- meteor-typescript-tests.ts
+++ meteor-typescript-tests.ts
@@ -4,8 +4,6 @@ import { Tinytest } from "meteor/tinytest";
 // Import and rename a variable exported by meteor-typescript.js.
 import { name as packageName } from "meteor/refapp:meteor-typescript";
 
-// Write your tests here!
-// Here is an example.
-Tinytest.add('meteor-typescript - example', function (test) {
+Tinytest.add("meteor-typescript - example", function (test) {
   test.equal(packageName, "meteor-typescript");
 });
--- package.js
+++ package.js
@@ -1,22 +1,31 @@
 Package.describe({
-  name: 'refapp:meteor-typescript',
-  version: '0.0.1',
-  summary: 'A Typescript compiler plugin for Meteor',
-  git: 'https://github.com/refapp/meteor-typescript',
-  documentation: 'README.md'
+  name: "refapp:meteor-typescript",
+  version: "0.0.1",
+  summary: "A Typescript compiler plugin for Meteor",
+  git: "https://github.com/refapp/meteor-typescript",
+  documentation: "README.md",
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.10');
-  api.use('typescript');
-  api.use('ecmascript');
-  api.mainModule('meteor-typescript.ts');
+Package.registerBuildPlugin({
+  name: "meteor-typescript",
+  use: ['typescript'],
+  sources: ["meteor-typescript.ts"],
+  npmDependencies: {
+    typescript: "3.9.2",
+  },
 });
 
-Package.onTest(function(api) {
-  api.use('typescript');
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('refapp:meteor-typescript');
-  api.mainModule('meteor-typescript-tests.ts');
+Package.onUse(function (api) {
+  api.versionsFrom("1.10");
+  api.use("typescript");
+  api.use("ecmascript");
+  api.mainModule("meteor-typescript.ts");
+});
+
+Package.onTest(function (api) {
+  api.use("typescript");
+  api.use("ecmascript");
+  api.use("tinytest");
+  api.use("refapp:meteor-typescript");
+  api.mainModule("meteor-typescript-tests.ts");
 });
