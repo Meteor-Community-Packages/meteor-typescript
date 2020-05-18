9a983291b3e821c8adae2afb52ba8d7cdbc91a05
Per Bergland <per@refapp.se>
2020-05-18 20:36:32 +0200

    adding types

--- meteor-typescript.ts
+++ meteor-typescript.ts
@@ -2,4 +2,31 @@
 
 // Variables exported by this module can be imported by other packages and
 // applications. See meteor-typescript-tests.js for an example of importing.
-export const name = 'meteor-typescript';
+export const name = "meteor-typescript";
+
+declare class Plugin {
+  static registerCompiler(
+    options: { extensions: string[]; filenames: string[] },
+    factory: () => any
+  ): void;
+}
+
+Plugin.registerCompiler(
+  {
+    extensions: ["ts", "tsx"],
+    filenames: ["tsconfig.json"],
+  },
+  function () {
+    return new TypeScriptCompiler();
+  }
+);
+
+class TypeScriptCompiler extends Compiler {
+  constructor() {
+    super();
+    console.log("constructor called");
+  }
+  processFilesForTarget(inputFiles: InputFile[]) {
+    inputFiles;
+  }
+}
