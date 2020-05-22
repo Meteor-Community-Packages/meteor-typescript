b94f1f0eff98ab7426804e6708fef6e3de2da928
Per Bergland <per@refapp.se>
2020-05-22 16:58:48 +0200

    publishing mods

--- package.js
+++ package.js
@@ -6,9 +6,11 @@ Package.describe({
   documentation: "README.md",
 });
 
+const COMPILER_VERSION = "0.0.1";
+
 Package.registerBuildPlugin({
   name: "meteor-typescript",
-  use: ["refapp:meteor-typescript-compiler"],
+  use: ["refapp:meteor-typescript-compiler@" + COMPILER_VERSION],
   sources: ["meteor-typescript-plugin.js"],
   npmDependencies: {
     typescript: "3.9.2",
@@ -19,5 +21,5 @@ Package.onUse(function (api) {
   api.use("isobuild:compiler-plugin@1.0.0");
   api.versionsFrom("1.10");
   api.use("ecmascript");
-  api.use("refapp:meteor-typescript-compiler");
+  api.use("refapp:meteor-typescript-compiler@" + COMPILER_VERSION);
 });
