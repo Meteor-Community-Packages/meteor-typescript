f74cef1164870c5871f7cfc1cb7a6c89ba2efce5
Per Bergland <per@refapp.se>
2024-04-29 14:21:41 +0200

    meteor 3rc0 for the sample app

--- tests/small-typescript-app/server/main.ts
+++ tests/small-typescript-app/server/main.ts
@@ -1,29 +1,29 @@
 import { Meteor } from 'meteor/meteor';
 import { LinksCollection } from '/imports/api/links';
 
-function insertLink(title: string, url: string) {
-  LinksCollection.insert({ title, url, createdAt: new Date() });
+async function insertLink(title: string, url: string) {
+  await LinksCollection.insertAsync({ title, url, createdAt: new Date() });
 }
 
-Meteor.startup(() => {
+Meteor.startup(async () => {
   // If the Links collection is empty, add some data.
-  if (LinksCollection.find().count() === 0) {
-    insertLink(
+  if (await LinksCollection.find().countAsync() === 0) {
+    await insertLink(
       'Do the Tutorial',
       'https://www.meteor.com/tutorials/react/creating-an-app'
     );
 
-    insertLink(
+    await insertLink(
       'Follow the Guide',
       'http://guide.meteor.com'
     );
 
-    insertLink(
+    await insertLink(
       'Read the Docs',
       'https://docs.meteor.com'
     );
 
-    insertLink(
+    await insertLink(
       'Discussions',
       'https://forums.meteor.com'
     );
